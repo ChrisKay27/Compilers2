c7920bab1b1875130ae6cd1569af379759856755
FireSpore <carstonschilds@gmail.com>
2016-01-09 16:25:34 -0800

    Added state for block comments which handles nesting

---
+++ scanner/BadCommentException.java
@@ -0,0 +1,10 @@
+package scanner;
+
+/**
+ * Created by Carston on 1/9/2016.
+ */
+public class BadCommentException extends RuntimeException {
+    public BadCommentException(String message) {
+        super(message);
+    }
+}
--- scanner/ScannerStateMachine.java
+++ scanner/ScannerStateMachine.java
@@ -12,13 +12,12 @@ import static stateMachine.Lexicon.isLetter;
  */
 public class ScannerStateMachine {
 
-    State init = new State(){
+    State init = new State() {
 
-        public Token consume(char c){
-            if(c <= 32){
+        public Token consume(char c) {
+            if (c <= 32) {
                 nextState = init;
-            }
-            else {
+            } else {
                 switch (c) {
                     case '{':
                         nextState = leftBrace;
@@ -66,14 +65,13 @@ public class ScannerStateMachine {
 
                 }
             }
-            if( isDigit(c) ){
+            if (isDigit(c)) {
                 nextState = num;
-                nextState.getSb().replace(0,nextState.getSb().length(),"");
+                nextState.getSb().replace(0, nextState.getSb().length(), "");
                 nextState.getSb().append(c);
-            }
-            else if( isLetter(c) ){
+            } else if (isLetter(c)) {
                 nextState = id;
-                nextState.getSb().replace(0,nextState.getSb().length(),"");
+                nextState.getSb().replace(0, nextState.getSb().length(), "");
                 nextState.getSb().append(c);
             }
             return null;
@@ -82,25 +80,20 @@ public class ScannerStateMachine {
     };
 
 
-    State num = new State(){
+    State num = new State() {
 
-        public Token consume(char c){
-            if( c >= 48 && c <= 57 ){
+        public Token consume(char c) {
+            if (c >= 48 && c <= 57) {
                 nextState = this;
                 sb.append(c);
                 return null;
-            }
-            else{
+            } else {
                 nextState = init;
                 String value = sb.toString();
-                sb.replace(0,sb.length(),"");
-                return new Token(Tokens.NUM,value);
+                sb.replace(0, sb.length(), "");
+                return new Token(Tokens.NUM, value);
             }
 
-        }
-
-        public State nextState(){
-            return nextState;
         }
     };
 
@@ -117,10 +110,6 @@ public class ScannerStateMachine {
             sb = new StringBuilder();
             return new Token(Tokens.ID, value);
         }
-
-        public State nextState() {
-            return nextState;
-        }
     };
 
     // remains in this state and ignores consumed characters until the end of line character is reached then returns
@@ -136,13 +125,14 @@ public class ScannerStateMachine {
             }
             return null;
         }
-        public State nextState(){
+
+        public State nextState() {
             return nextState;
         }
     };
     // state could be a dash for negation or subtraction or could be a dash to start a line comment
     State minus = new State() {
-        public Token consume(char c){
+        public Token consume(char c) {
             if (c == Lexicon.MINUS) {
                 //line comment
                 nextState = lineComment;
@@ -155,250 +145,310 @@ public class ScannerStateMachine {
             }
             return null;
         }
-
-        public State nextState(){
-            return nextState;
-        }
     };
 
-    State divOrComment = new State(){
+    State divOrComment = new State() {
 
-        public Token consume(char c){
+        public Token consume(char c) {
 
-            if(c == '/'){
-                sb.replace(0,sb.length(),"");
-                return new Token(Tokens.DIV,null);
+            if (c == '/') {
+                sb.replace(0, sb.length(), "");
+                return new Token(Tokens.DIV, null);
 
-            }else if(c == '*' && sb.toString().equals("/")){
+            } else if (c == '*' && sb.toString().equals("/")) {
 
-            }
-            else{
+            } else {
                 throw new BadTokenException("Bad Token: " + sb.toString() + c);
             }
 
             return null;
         }
-
-        public State nextState(){
-            return nextState;
-        }
     };
 
-    State lessThanOrEq = new State(){
+    State lessThanOrEq = new State() {
 
-        public Token consume(char c){
-            if(c == '='){
-                sb.replace(0,sb.length(),"");
-                return new Token(Tokens.LTEQ,null);
-            }
-            else{
-                sb.replace(0,sb.length(),"");
-                return new Token(Tokens.LT,null);
+        public Token consume(char c) {
+            if (c == '=') {
+                sb.replace(0, sb.length(), "");
+                return new Token(Tokens.LTEQ, null);
+            } else {
+                sb.replace(0, sb.length(), "");
+                return new Token(Tokens.LT, null);
             }
         }
 
-        public State nextState(){
+        public State nextState() {
             return nextState;
         }
     };
 
 
-    State greaterThanOrEq = new State(){
+    State greaterThanOrEq = new State() {
 
-        public Token consume(char c){
-            if(c == '='){
-                sb.replace(0,sb.length(),"");
-                return new Token(Tokens.GTEQ,null);
-            }
-            else{
-                sb.replace(0,sb.length(),"");
-                return new Token(Tokens.GT,null);
+        public Token consume(char c) {
+            if (c == '=') {
+                sb.replace(0, sb.length(), "");
+                return new Token(Tokens.GTEQ, null);
+            } else {
+                sb.replace(0, sb.length(), "");
+                return new Token(Tokens.GT, null);
             }
         }
 
-        public State nextState(){
+        public State nextState() {
             return nextState;
         }
     };
 
-    State orElse = new State(){
+    State orElse = new State() {
 
-        public Token consume(char c){
+        public Token consume(char c) {
 
-            if(c == '|'){
+            if (c == '|') {
 
                 sb.append('|');
-                if(sb.toString().equals("||")){
+                if (sb.toString().equals("||")) {
                     nextState = init;
 
                 }
 
-            }else if(sb.toString().equals("|")){
+            } else if (sb.toString().equals("|")) {
                 throw new BadTokenException("Bad Token: " + sb.toString() + c);
             }
 
             return null;
         }
 
-        public State nextState(){
+        public State nextState() {
             return nextState;
         }
     };
 
-    State colonEq = new State(){
+    State colonEq = new State() {
 
-        public Token consume(char c){
+        public Token consume(char c) {
 
-            if(c == '='){
-                if( sb.toString().equals(":=") ){
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ASSIGN,null);
+            if (c == '=') {
+                if (sb.toString().equals(":=")) {
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ASSIGN, null);
                 }
 
                 sb.append('=');
-                if(sb.toString().equals("||")){
+                if (sb.toString().equals("||")) {
                     nextState = init;
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ORELSE,null);
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ORELSE, null);
                 }
-            }else{
+            } else {
 
             }
             return null;
         }
     };
 
-    State leftBrace = new State(){
+    State leftBrace = new State() {
 
-        public Token consume(char c){
+        public Token consume(char c) {
 
-            if(c == '='){
-                if( sb.toString().equals(":=") ){
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ASSIGN,null);
+            if (c == '=') {
+                if (sb.toString().equals(":=")) {
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ASSIGN, null);
                 }
 
                 sb.append('=');
-                if(sb.toString().equals("||")){
+                if (sb.toString().equals("||")) {
                     nextState = init;
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ORELSE,null);
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ORELSE, null);
                 }
-            }else{
+            } else {
 
             }
             return null;
         }
     };
-    State rightBrace = new State(){
+    State rightBrace = new State() {
 
-        public Token consume(char c){
+        public Token consume(char c) {
 
-            if(c == '='){
-                if( sb.toString().equals(":=") ){
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ASSIGN,null);
+            if (c == '=') {
+                if (sb.toString().equals(":=")) {
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ASSIGN, null);
                 }
 
                 sb.append('=');
-                if(sb.toString().equals("||")){
+                if (sb.toString().equals("||")) {
                     nextState = init;
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ORELSE,null);
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ORELSE, null);
                 }
-            }else{
+            } else {
 
             }
             return null;
         }
     };
-    State leftParen = new State(){
+    State leftParen = new State() {
 
-        public Token consume(char c){
+        public Token consume(char c) {
 
-            if(c == '='){
-                if( sb.toString().equals(":=") ){
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ASSIGN,null);
+            if (c == '=') {
+                if (sb.toString().equals(":=")) {
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ASSIGN, null);
                 }
 
                 sb.append('=');
-                if(sb.toString().equals("||")){
+                if (sb.toString().equals("||")) {
                     nextState = init;
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ORELSE,null);
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ORELSE, null);
                 }
-            }else{
+            } else {
 
             }
             return null;
         }
     };
-    State plus = new State(){
+    State plus = new State() {
 
-        public Token consume(char c){
+        public Token consume(char c) {
 
-            if(c == '='){
-                if( sb.toString().equals(":=") ){
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ASSIGN,null);
+            if (c == '=') {
+                if (sb.toString().equals(":=")) {
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ASSIGN, null);
                 }
 
                 sb.append('=');
-                if(sb.toString().equals("||")){
+                if (sb.toString().equals("||")) {
                     nextState = init;
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ORELSE,null);
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ORELSE, null);
                 }
-            }else{
+            } else {
 
             }
             return null;
         }
     };
-    State rightParen = new State(){
+    State rightParen = new State() {
 
-        public Token consume(char c){
+        public Token consume(char c) {
 
-            if(c == '='){
-                if( sb.toString().equals(":=") ){
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ASSIGN,null);
+            if (c == '=') {
+                if (sb.toString().equals(":=")) {
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ASSIGN, null);
                 }
 
                 sb.append('=');
-                if(sb.toString().equals("||")){
+                if (sb.toString().equals("||")) {
                     nextState = init;
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ORELSE,null);
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ORELSE, null);
                 }
-            }else{
+            } else {
 
             }
             return null;
         }
     };
 
-    State minusOrLineComment = new State(){
+    State minusOrLineComment = new State() {
 
-        public Token consume(char c){
+        public Token consume(char c) {
 
-            if(c == '='){
-                if( sb.toString().equals(":=") ){
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ASSIGN,null);
+            if (c == '=') {
+                if (sb.toString().equals(":=")) {
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ASSIGN, null);
                 }
 
                 sb.append('=');
-                if(sb.toString().equals("||")){
+                if (sb.toString().equals("||")) {
                     nextState = init;
-                    sb.replace(0,sb.length(),"");
-                    return new Token(Tokens.ORELSE,null);
+                    sb.replace(0, sb.length(), "");
+                    return new Token(Tokens.ORELSE, null);
                 }
-            }else{
+            } else {
 
             }
             return null;
+        }
+    };
+    // reached upon receiving '/*'
+    // ignores all characters other than * and /
+    // * -> goes to end comment state
+    // / -> enters nestedComment state
+    // OTHER -> loopback
+    State blockComment = new State() {
+
+        private int count;
+
+        public Token consume(char c) {
+            if (c == '*') {
+                // potentially end of comment
+                nextState = this.endComment;
+            } else if (c == '/') {
+                // potentially start of nested commend
+                nextState = this.nestedComment;
+            } else {
+                // any other character inside a comment is ignored
+                nextState = this;
+            }
+            return null;
+        }
+
+        // reached upon receiving '/' from the block comment state
+        // * -> increment the counter and return to block comment state
+        // OTHER ignore and return to the block comment state
+        private State nestedComment = new State() {
+
+            public Token consume(char c) {
+                if (c == '*') {
+                    // go back to blockComment and return the comment token
+                    increment();
+                }
+                nextState = blockComment;
+                return null;
+            }
+
+        };
+
+        // reached upon receiving '*' from the blockComment state
+        // / -> end the comment and return the comment token
+        // OTHER -> returns to blockComment state
+        private State endComment = new State() {
+            public Token consume(char c) {
+                if (c == '/') {
+                    if (countZero()) {
+                        nextState = init;
+                        return Token.COMMENT_TOKEN;
+                    } else {
+                        decrement();
+                        nextState = blockComment;
+                    }
+                } else {
+                    nextState = blockComment;
+                }
+                return null;
+            }
+        };
+
+        private void increment() {
+            count++;
+        }
+
+        private void decrement() {
+            count--;
+        }
+
+        private boolean countZero() {
+            return count == 0;
         }
     };
 
